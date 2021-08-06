0dfc4c3e00c4f9590a3a285a52f726372198e448
amroczeK <mroczek9990@gmail.com>
2021-08-06 11:14:00 +0800

    implemented mongodb and service healthchecks

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using dotnet_webapi.Config;
 using dotnet_webapi.Repositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -34,11 +38,11 @@ namespace dotnet_webapi
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
 
             services.AddSingleton<IMongoClient>(ServiceProvider => 
             {
-                var settings = Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
-                return new MongoClient(settings.ConnectionString);
+                return new MongoClient(mongoDbSettings.ConnectionString);
             });
             // Singleton: One copy of the instance of a type across the entire lifetime of service
             // Only one will be created and re-used whenever it's needed
@@ -53,6 +57,9 @@ namespace dotnet_webapi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet_webapi", Version = "v1" });
             });
+
+            // Application/service healthcheck to Mongo Database
+            services.AddHealthChecks().AddMongoDb(mongoDbSettings.ConnectionString, name:"mongodb", timeout: TimeSpan.FromSeconds(3), tags: new[]{"ready"});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +81,36 @@ namespace dotnet_webapi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                // https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-5.0
+                // https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks
+                
+                // Verifies database is ready to service requests
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions{
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async(context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new{
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new {
+                                    name = entry.Key,
+                                    status = entry.Value.Status.ToString(),
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                    duration = entry.Value.Duration.ToString()
+                                })
+                            }
+                        );
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                    
+                });
+
+                // Verifies service is alive/up and running
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions{
+                    Predicate = (_) => false
+                });
             });
         }
     }
